dbc2a4f246f6c097d48da78ba1a158b140a90c21
HeavenlyLich <130435636+HeavenlyLich@users.noreply.github.com>
2026-03-31 22:16:55 +0300

    Fix diagonal win condition
    Closes #2

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class TicTacToeBuggy(TicTacToeBase):
 
     def check_winner(self):
         # BUG: Відсутні діагоналі
-        win_coords = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8)]
+        win_coords = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]            # ВИПРАВЛЕНО: Діагоналі
         for combo in win_coords:
             if self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] != " ":
                 return True
